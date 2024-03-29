9a71966cb3423a6c05e4c4ef9e2cde0d57789d94
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-03-29 15:57:14 +0530

    Update Choose.jsx

--- src/components/Choose/Choose.jsx
+++ src/components/Choose/Choose.jsx
@@ -77,8 +77,7 @@ const Choose = () => {
                     whileHover={{ scale: 1.1 }}
                     className="flex items-center flex-col gap-4 bg-white p-8 rounded-md"
                 >
-                    <div className="text-4xl text-Teal">{}</div>
-                    <div>{category}</div>
+                    
                     <a href="" className="text-sm text-gray">
                         View More
                     </a>
